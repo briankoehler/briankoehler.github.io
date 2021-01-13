a79a89085558f72ed76e8362c176917c33f7c5db
briankoehler <briankoehler@ufl.edu>
2021-01-13 01:40:13 -0500

    Initial TRON commit

--- main.js
+++
@@ -1,9 +0,0 @@
-function myFunction() {
-    let x = document.getElementsByClassName("nav-tabs");
-    if (x.className === "nav-tabs") {
-        x.className += " responsive";
-    }
-    else {
-        x.className = "nav-tabs";
-    }
-}
