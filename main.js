48498fcb2ca8695c669f64ee409f9696a4301e03
briankoehler <briankoehler@ufl.edu>
2021-02-05 02:48:49 -0500

    Navbar fixed on mobile

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
 
+let prevScrollPosition = window.pageYOffset;
+window.onscroll = () => {
+    let currentScrollPosition = window.pageYOffset;
+    if (prevScrollPosition < currentScrollPosition) {
+        document.getElementById('navbar').style.top = '-11vh';
+    }
+    else {
+        document.getElementById('navbar').style.top = '0vh';
+    }
+    prevScrollPosition = currentScrollPosition;
+}
